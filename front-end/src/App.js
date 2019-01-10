99fd6264afbb2e1f1613d8e666d78658e3aa0c8b
Noah Amar <ne.amar@outlook.com>
2019-01-10 13:37:21 -0600

    added wrapping div

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -28,6 +28,7 @@ class App extends Component {
 
   render() {
     return (
+      <div>
       <div className="App">
         <Payment />
         <Settings />
@@ -40,7 +41,7 @@ class App extends Component {
         <ProfilePic />
         <LandingPageButtons />
         <SignupLogin />
-      <div>
+      </div>
       <div>
         <PerfectScrollbar>
           <LandingPage />
