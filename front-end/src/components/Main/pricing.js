c51e60363c5f0f8abfb39b4bf235dc41e4b55a69
Noah Amar <ne.amar@outlook.com>
2019-01-16 15:10:39 -0600

    capitalization

--- front-end/src/components/Main/pricing.js
+++ front-end/src/components/Main/pricing.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 
 
-import Prices from '../prices';
-import StickyTop from '../topBar';
+import Prices from './Prices';
+import StickyTop from '../Navigation/topBar';
 
 
 
