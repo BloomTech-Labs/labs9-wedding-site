b582dfad51c11ef35649c366d72aa972572b4e00
Arthur <aabatesj@gmail.com>
2019-01-22 15:40:55 -0500

    design page changes
    changed some images, added redirect t signup in lightbox

--- front-end/src/components/Main/designs.js
+++ front-end/src/components/Main/designs.js
@@ -1,27 +1,22 @@
 import React from 'react';
-import { render } from 'react-dom';
 import Gallery from 'react-photo-gallery';
 import Lightbox from 'react-images';
+import styled from 'styled-components';
+
+const DesignsContainer = styled.div`
+margin: auto;
+margin-top: 200px;
+margin-bottom: 80px;
+background-color: white;
+width: 87.4%;
+display:flex;
+justify-content: space-between;
+`
 
 const photos = [
-    { src: 'http://zanatlantean.com/shared/2014/02/catoandaj-sreenshot.png', width: 5, height: 3 },
-    { src: 'https://media-api.xogrp.com/images/0510a356-b2c9-4e31-9a6d-5bbbe5c79f89~rs_600.h', width: 1, height: 1 },
-    { src: 'https://www.wpsolver.com/wp-content/uploads/2012/10/just-married.png', width: 3, height: 4 },
-    { src: 'https://withjoy.com/assets/img/hero2018/hero-brannan.png', width: 3, height: 4 },
-    { src: 'http://myday.ynet.co.il/en/wp-content/blogs.dir/en/files/2013/03/Screen-shot-2013-03-17-at-9.21.04-AM.png', width: 7, height: 4 },
-    { src: 'https://blog.beautheme.com/wp-content/uploads/2016/05/Wedding-Theme-Wordpress-03.jpg', width: 6, height: 3 },
-    { src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5sk4kYRVYyp6JCW1dCHBpTjDJffc9SwCEOOqsz1uxuaLs0a-tAg', width: 3, height: 4 },
-    { src: 'https://static.wixstatic.com/media/4afcc986759fd062e04a9f06d98bb17939a12609d27ff9ce69a6ae9885a3aa62.jpg', width: 6, height: 3 },
-    { src: 'http://www.an-elegant-affair.net/images/Wedding%20Website.png', width: 4, height: 3 },
-    { src: 'http://zanatlantean.com/shared/2014/02/catoandaj-sreenshot.png', width: 5, height: 3 },
-    { src: 'https://media-api.xogrp.com/images/0510a356-b2c9-4e31-9a6d-5bbbe5c79f89~rs_600.h', width: 1, height: 1 },
-    { src: 'https://www.wpsolver.com/wp-content/uploads/2012/10/just-married.png', width: 3, height: 4 },
-    { src: 'https://withjoy.com/assets/img/hero2018/hero-brannan.png', width: 3, height: 4 },
-    { src: 'http://myday.ynet.co.il/en/wp-content/blogs.dir/en/files/2013/03/Screen-shot-2013-03-17-at-9.21.04-AM.png', width: 7, height: 4 },
-    { src: 'https://blog.beautheme.com/wp-content/uploads/2016/05/Wedding-Theme-Wordpress-03.jpg', width: 6, height: 3 },
-    { src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5sk4kYRVYyp6JCW1dCHBpTjDJffc9SwCEOOqsz1uxuaLs0a-tAg', width: 3, height: 4 },
-    { src: 'https://static.wixstatic.com/media/4afcc986759fd062e04a9f06d98bb17939a12609d27ff9ce69a6ae9885a3aa62.jpg', width: 6, height: 3 },
-    { src: 'http://www.an-elegant-affair.net/images/Wedding%20Website.png', width: 4, height: 3 }
+    { src: 'https://cdn.freshdesignweb.com/wp-content/uploads/glanz-html-wedding-template.jpg', width: 4 },
+    { src: 'https://cdn.freshdesignweb.com/wp-content/uploads/belle-responsive-wedding-template.jpg', width: 4 },
+    { src: 'https://cdn.freshdesignweb.com/wp-content/uploads/site/newlyweds-html-wedding-template.jpg', width: 4 },
 ];
 
 
@@ -33,6 +28,7 @@ class Design extends React.Component {
         this.openLightbox = this.openLightbox.bind(this);
         this.gotoNext = this.gotoNext.bind(this);
         this.gotoPrevious = this.gotoPrevious.bind(this);
+        this.goToSignUp = this.goToSignUp.bind(this);
     }
     openLightbox(event, obj) {
         this.setState({
@@ -56,18 +52,23 @@ class Design extends React.Component {
             currentImage: this.state.currentImage + 1,
         });
     }
+    goToSignUp() {
+        let path = './signup';
+        this.props.history.push(path);
+    }
     render() {
         return (
-            <div>
+            <DesignsContainer>
                 <Gallery photos={photos} onClick={this.openLightbox} />
                 <Lightbox images={photos}
                     onClose={this.closeLightbox}
                     onClickPrev={this.gotoPrevious}
                     onClickNext={this.gotoNext}
                     currentImage={this.state.currentImage}
+                    onClickImage={this.goToSignUp}
                     isOpen={this.state.lightboxIsOpen}
                 />
-            </div>
+            </DesignsContainer>
         )
     }
 }
