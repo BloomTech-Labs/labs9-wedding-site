8329c61ab4b64a6b65f6198560cf1359c3f1b783
bkwillia052 <bkwillia052@gmail.com>
2019-01-23 13:44:34 -0600

    Added PublicInvite component; a page to display the couple's invitation

---
+++ front-end/src/components/Client/PublicInvite.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import MainContent from './components/Main/MainContent';
+import { withRouter } from 'react-router';
+
+
+class PublicInvite extends Component {
+        state = {
+            
+        }
+  render() {
+    return (
+      <div>
+        <MainContent {...this.props}/>
+      </div>
+    );
+  }
+}
+
+export default withRouter(App);
