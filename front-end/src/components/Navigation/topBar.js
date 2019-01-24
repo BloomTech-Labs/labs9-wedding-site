6f0dc85f39f4261c054a442e85c1e2e7ccdca870
Noah Amar <ne.amar@outlook.com>
2019-01-24 01:33:26 -0600

    merged

--- front-end/src/components/Navigation/topBar.js
+++ front-end/src/components/Navigation/topBar.js
@@ -18,13 +18,8 @@ const StickyTop = (props) => {
         <LandingButtonsExp />
       </div>
       <div className="centerNav">
-<<<<<<< HEAD
-        <Link style={{textDecoration:'none !important'}} to='/'>
-          <h1 className="siteName">Beloved</h1> 
-=======
         <Link to='/'>
           <h1 className="siteName">Beloved</h1>
->>>>>>> 3745c6d215594e635b0323aaff53d8a8c0afb8a9
         </Link>
       </div>
       <div className="rightNav">
