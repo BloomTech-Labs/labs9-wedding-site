fa8b6fb7d4ddc1e30185b996d79524ae11652efe
Hargobind Atwal <ethaipath@gmail.com>
2019-01-28 09:41:15 -0800

    Condensed index.js file

--- back-end/index.js
+++ back-end/index.js
@@ -1,12 +1,6 @@
-const server = require('./api/server');
 require('dotenv').config();
-
+const server = require('./api/server');
 const PORT = process.env.PORT || 8888;
-
-
-
-
-
-
-server.listen(PORT, ()=> console.log(`Server is running on Port ${PORT}`))
-
+server.listen(PORT, () => console.log(
+    `Server is running on Port ${PORT}`
+))
