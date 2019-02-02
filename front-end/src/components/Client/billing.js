9cb21bafbcb5094416a2bb5a3fb6de7ec42330a1
bkwillia052 <bkwillia052@gmail.com>
2019-02-02 15:25:23 -0600

    Updated billing.js from functional to class component

--- front-end/src/components/Client/billing.js
+++ front-end/src/components/Client/billing.js
@@ -5,17 +5,30 @@ import Sidebar from './clientNav';
 import './billing.css';
 
 
-function Payment() {
-    return (
-        <div className="billing">
-            <Sidebar />
-            <div className="billingContainer">
-                <StripeProvider apiKey="pk_test_M1Y5kyDDSB7dOAWXIhzOOqMV">
-                    <Checkout />
-                </StripeProvider>
+class Payment extends React.Component {
+
+    state = {}
+
+    componentDidMount(){
+
+    }
+
+    render(){
+
+        return (
+            <div className="billing">
+                <Sidebar />
+                <div className="billingContainer">
+                    <StripeProvider apiKey="pk_test_M1Y5kyDDSB7dOAWXIhzOOqMV">
+                        <Checkout />
+                    </StripeProvider>
+                </div>
             </div>
-        </div>
-    );
+        );
+
+
+    }
+    
 }
 
 export default Payment;
