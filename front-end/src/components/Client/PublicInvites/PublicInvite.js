884737ecd1f33a5fda7440bbed4f8502e769c77f
bkwillia052 <bkwillia052@gmail.com>
2019-02-07 10:10:13 -0600

    Started adding responsiveness

--- front-end/src/components/Client/PublicInvites/PublicInvite.js
+++ front-end/src/components/Client/PublicInvites/PublicInvite.js
@@ -88,7 +88,7 @@ class PublicInvite extends Component {
         <div className="invite-cont">
 
               <div className="header-name">
-                <div className="couple-names">{this.state.partner1.first_name} & {this.state.partner2.first_name}</div>
+                <div className="couples-names">{this.state.partner1.first_name} & {this.state.partner2.first_name}</div>
               </div>
 
               <div className="save-date">
@@ -156,7 +156,7 @@ class PublicInvite extends Component {
         <div className="part-three">
 
               <div className="story">
-                <span>Our Love Story</span>
+                <span className="story-title">Our Love Story</span>
                 <div className='paragraph'>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                   eiusmod tempor incididunt<br/> ut labore et dolore magna aliqua. Ut
